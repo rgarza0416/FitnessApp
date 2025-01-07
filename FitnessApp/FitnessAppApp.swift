8f9c709383c3ac433c82a90c875ccf1a1e1f6213
Ricardo Garza <rgarza0416@yahoo.com>
2025-01-07 11:09:30 -0600

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Ricardo Garza on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
