09196081820cc14c1282cbaca632d1c9764c3c9a
Ricardo Garza <rgarza0416@yahoo.com>
2025-01-07 14:24:49 -0600

    Created homeView

--- FitnessApp/HomeView.swift
+++ FitnessApp/HomeView.swift
@@ -8,8 +8,70 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var calories: Int = 200
+    @State var active: Int = 55
+    @State var stand: Int = 8
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: false) {
+            VStack {
+                Text("Welcome")
+                    .font(.largeTitle)
+                    .padding()
+                
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Calories")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.red)
+                            
+                            Text("123 Kcal")
+                                .bold()
+                        }
+                        .padding(.bottom)
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Active")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.green)
+                            
+                            Text("52 mins")
+                                .bold()
+                        }
+                        .padding(.bottom)
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Stand")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.blue)
+                            
+                            Text("8 Hours")
+                                .bold()
+                        }
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    ZStack {
+                        ProgressCircleView(progress: $calories, goal: 600, color: .red)
+                        ProgressCircleView(progress: $active, goal: 60, color: .green)
+                            .padding(20)
+                        ProgressCircleView(progress: $stand, goal: 12, color: .blue)
+                            .padding(40)
+                    }
+                    .padding(.horizontal)
+                    Spacer()
+                }
+                .padding()
+            }
+        }
     }
 }
 
