693db8dc60a31c8aa2ba29a99871a9ad2f63de11
Ricardo Garza <rgarza0416@yahoo.com>
2025-01-22 16:36:08 -0600

    integreated RevenueCat

--- FitnessApp/FitnessAppApp.swift
+++ FitnessApp/FitnessAppApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import RevenueCat
 
 @main
 struct FitnessAppApp: App {
+    
+    init() {
+            Purchases.logLevel = .debug
+            Purchases.configure(withAPIKey: "appl_PWtJxVqsMgGpVEwBMyftIDunzcy")
+    }
+    
     var body: some Scene {
         WindowGroup {
             FitnessTabView()
+            //    .onAppear {
+             //       Purchases.shared.getOfferings { (offerings, error) in
+           //                     if let packages = offerings?.current?.availablePackages {
+            //                        print(packages.first?.offeringIdentifier)
+            //                    }
+            //                }
+            //    }
+        
+        
             //HomeView(viewModel: HomeViewModel())
         }
     }
